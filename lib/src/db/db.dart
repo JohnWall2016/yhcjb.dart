68e50f9099390afac530169117088f08660cf779
JohnWall2016 <5672618@qq.com>
2019-05-05 17:02:28 +0800

    update

--- lib/src/db/db.dart
+++ lib/src/db/db.dart
@@ -8,3 +8,52 @@ Future<MySqlConnection> getDbConnection() async {
       host: conf['host'], port: conf['port'], user: conf['user'], password: conf['password'], db: conf['db']));
   return conn;
 }
+
+class Table {
+  final String name;
+  const Table({this.name});
+}
+
+class Field {
+  final String name;
+  const Field({this.name});
+}
+
+@Table(name: '2019年度扶贫办民政残联历史数据')
+class FpHistoryBook {
+  @Field(name: '序号')
+  int no;
+
+  @Field(name: '乡镇街')
+  String xzj;
+
+  @Field(name: '村社区')
+  String csq;
+
+  @Field(name: '地址')
+  String address;
+
+  @Field(name: '姓名')
+  String name;
+
+  @Field(name: '身份证号码')
+  String idcard;
+
+  @Field(name: '出生日期')
+  String birthDay;
+
+  @Field(name: '人员类型')
+  String type;
+
+  @Field(name: '类型细节')
+  String detail;
+
+  @Field(name: '数据月份')
+  String date;
+}
+
+class Model<T> {
+  Model() {
+    
+  }
+}
