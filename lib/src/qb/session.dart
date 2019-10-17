a10b5ff69e41e334d8ab1c45e8de790aa1785fed
JohnWall2016 <5672618@qq.com>
2019-10-17 16:36:23 +0800

    update

--- lib/src/qb/session.dart
+++ lib/src/qb/session.dart
@@ -1,6 +1,7 @@
 import 'dart:mirrors';
 import 'package:xml/xml.dart';
 import '../xml/node.dart';
+import '../xml/xml.dart';
 import './_config.dart';
 
 class Xml {
@@ -9,57 +10,46 @@ class Xml {
   const Xml({this.name, this.ignored});
 }
 
-class Envelop<T extends RequestBody> {
-  Params<RequestHeader> header;
-  Params<T> body;
+class RequestEnvelop<T extends RequestBody> {
+  RequestHeader header;
+  T body;
 
-  Envelop(T body) {
-    this.header = Params(RequestHeader(funid: body.funid));
-    this.body = Params(body);
+  RequestEnvelop(T body) {
+    this.header = RequestHeader(funid: body.funid);
+    this.body = body;
   }
 
   XmlNode toXml() {
     return (Element('soap:Envelope', {
       'xmlns:soap': 'http://schemas.xmlsoap.org/soap/envelope/',
       'soap:encodingStyle': 'http://schemas.xmlsoap.org/soap/encoding/'
-    })..children.add(
-      Element('soap:Header')..children.add(
-        Element('in:system', {
-          'xmlns:in': 'http://www.molss.gov.cn/'
-        })..children.addAll(header.toNodes())
-      )
-    )..children.add(
-      Element('soap:Body')..children.add(
-        Element('in:business', {
-          'xmlns:in': 'http://www.molss.gov.cn/'
-        })..children.addAll(body.toNodes())
-      )
-    )).toXmlNode();
+    })
+          ..children.add(Element('soap:Header')
+            ..children.add(
+                Element('in:system', {'xmlns:in': 'http://www.molss.gov.cn/'})
+                  ..children.addAll(header.toNodes())))
+          ..children.add(Element('soap:Body')
+            ..children.add(
+                Element('in:business', {'xmlns:in': 'http://www.molss.gov.cn/'})
+                  ..children.addAll(body.toNodes()))))
+        .toXmlNode();
   }
 
   String toXmlString() => toXml().toXmlString();
 }
 
-abstract class Serializable{}
-
-class Params<T> {
-  final T params;
-
-  Params(this.params);
-
-  Iterable<Node> toNodes() sync*{
+class Paramable {
+  Iterable<Node> toNodes() sync* {
     var map = <String, String>{};
-    InstanceMirror inst = reflect(this.params);
+    InstanceMirror inst = reflect(this);
     _toMap(inst, inst.type, map);
     for (var key in map.keys) {
-      yield Element('para', {
-        key: map[key]
-      });
+      yield Element('para', {key: map[key]});
     }
   }
 
   void _toMap(InstanceMirror inst, ClassMirror clazz, Map<String, String> map) {
-    if (!clazz.isSubtypeOf(reflectType(Serializable))) return;
+    if (!clazz.isSubtypeOf(reflectType(Paramable))) return;
 
     _toMap(inst, clazz.superclass, map);
 
@@ -87,7 +77,7 @@ class Params<T> {
   }
 }
 
-class UserInfo extends Serializable{
+class UserInfo extends Paramable {
   String usr, pwd;
   UserInfo(String usr, String pwd) {
     if (usr == null || pwd == null) {
@@ -98,19 +88,19 @@ class UserInfo extends Serializable{
 }
 
 class RequestHeader extends UserInfo {
-  final funid;
+  final String funid;
   RequestHeader({usr, pwd, this.funid}) : super(usr, pwd);
 }
 
-class RequestBody extends Serializable {
+class RequestBody extends Paramable {
   @Xml(ignored: true)
   final String funid;
-  
+
   final String functionid;
   RequestBody(this.funid, this.functionid);
 }
 
-class Sncbrycx extends RequestBody {
+class SncbrycxRequest extends RequestBody {
   int startrow = 1;
   int row_count = -1;
   int pagesize = 500;
@@ -119,7 +109,175 @@ class Sncbrycx extends RequestBody {
   @Xml(ignored: true)
   String idcard;
 
-  Sncbrycx(this.idcard) : super('F00.01.03', 'F27.06') {
+  SncbrycxRequest(this.idcard) : super('F00.01.03', 'F27.06') {
     clientsql = '( aac002 = &apos;$idcard&apos;)';
   }
+}
+
+final ResultableType = reflectType(Resultable);
+final NumType = reflectType(num);
+
+class Resultable {
+  static convert(XmlNode node) {
+    var map = <String, dynamic>{};
+    for (var node in node?.children ?? []) {
+      if (node is XmlElement) {
+        if (node.name.local == 'result') {
+          for (var attr in node.attributes) {
+            map[attr.name.local] = attr.value;
+            break;
+          }
+        } else if (node.name.local == 'resultset') {
+          String key = getAttribute(node, 'name');
+          if (key != null) {
+            map[key] = [];
+            node.children.forEach((n) {
+              if (n is XmlElement) {
+                if (n.name.local == 'row') {
+                  var rowmap = <String, String>{};
+                  for (var attr in n.attributes) {
+                    rowmap[attr.name.local] = attr.value;
+                  }
+                  map[key].add(rowmap);
+                }
+              }
+            });
+          }
+        }
+      }
+    }
+    return map;
+  }
+
+  static T fromNode<T extends Resultable>(XmlNode node) {
+    var map = convert(node);
+    var clazz = reflectType(T) as ClassMirror;
+    var inst = clazz.newInstance(Symbol(''), []);
+    _fromMap(map, inst, clazz);
+    return inst.reflectee;
+  }
+
+  static _fromMap(
+      Map<String, dynamic> map, InstanceMirror inst, ClassMirror clazz) {
+    if (!clazz.isSubtypeOf(ResultableType)) return;
+
+    _fromMap(map, inst, clazz.superclass);
+
+    clazz.declarations.values.forEach((decl) {
+      if (decl is VariableMirror) {
+        String name;
+        var meta = decl.metadata.firstWhere(
+            (metadataMirror) => metadataMirror.reflectee is Xml,
+            orElse: () => null);
+        if (meta != null) {
+          Xml xml = meta.reflectee;
+          if (xml.ignored != null && xml.ignored) {
+            return;
+          } else if (xml.name != null && xml.name.isNotEmpty) {
+            name = xml.name;
+          }
+        }
+        Symbol sName = decl.simpleName;
+        name ??= MirrorSystem.getName(sName);
+        if (map.containsKey(name)) {
+          var value = map[name];
+          if (value == null) {
+            inst.setField(sName, value);
+            return;
+          }
+          var type = decl.type;
+          if (value is List && type.simpleName == #List) {
+            var argType =
+                type.typeArguments.isNotEmpty ? type.typeArguments.first : null;
+            if (argType != null &&
+                argType.simpleName != #dynamic &&
+                argType.isSubtypeOf(ResultableType)) {
+              var listMirror =
+                  reflectType(List, [argType.reflectedType]) as ClassMirror;
+              var list = listMirror.newInstance(Symbol(''), []);
+              value.forEach((v) {
+                var argClazz = (argType as ClassMirror);
+                var argInst = argClazz.newInstance(Symbol(''), []);
+                Resultable._fromMap(v, argInst, argClazz);
+                list.reflectee.add(argInst.reflectee);
+              });
+              inst.setField(sName, list.reflectee);
+              return;
+            }
+          }
+          if (value is Map && type.isSubtypeOf(ResultableType)) {
+            var argClazz = (type as ClassMirror);
+            var argInst = argClazz.newInstance(Symbol(''), []);
+            Resultable._fromMap(value, argInst, argClazz);
+            inst.setField(sName, argInst.reflectee);
+          } else if (reflectType(value.runtimeType).isAssignableTo(type)) {
+            inst.setField(sName, value);
+          } else if (value is String && type.isSubtypeOf(NumType)) {
+            var n = num.tryParse(value);
+            if (n != null) {
+              inst.setField(sName, n);
+            }
+          }
+        }
+      }
+    });
+  }
+}
+
+class ResponseEnvelop<T extends ResponseBody> {
+  ResponseHeader header;
+  T body;
+
+  ResponseEnvelop();
+
+  static ResponseEnvelop<T> fromXmlDocument<T extends ResponseBody>(
+      XmlDocument doc) {
+    if (doc != null) {
+      var env = findChild(doc, 'Envelope');
+      if (env != null) {
+        var res = ResponseEnvelop<T>();
+        var header = findChild(env, 'Header');
+        if (header != null) {
+          res.header = Resultable.fromNode<ResponseHeader>(header);
+        }
+        var body = findChild(env, 'Body');
+        if (body != null) {
+          var business = findChild(body, 'business');
+          if (business != null) {
+            res.body = Resultable.fromNode<T>(business);
+          }
+        }
+        return res;
+      }
+    }
+    return null;
+  }
+
+  static ResponseEnvelop<T> fromXmlString<T extends ResponseBody>(String xml) {
+    return ResponseEnvelop.fromXmlDocument(parse(xml));
+  }
+}
+
+class ResponseHeader extends Resultable {
+  String sessionID, message;
+}
+
+class ResponseBody extends Resultable {
+  String result;
+}
+
+class SncbrycxItem extends Resultable {
+  @Xml(name: 'aac003')
+  String name;
+
+  @Xml(name: 'aac002')
+  String idcard;
+
+  @Xml(name: 'aab300')
+  String sbjg;
+}
+
+class SncbrycxResponse extends ResponseBody {
+  List<SncbrycxItem> querylist;
+  int row_count;
 }
--- test/xml_test.dart
+++ test/xml_test.dart
@@ -1,6 +1,62 @@
+import 'package:xml/xml.dart';
+import 'package:yhcjb/src/xml/xml.dart';
 import 'package:yhcjb/yhqb.dart';
 
 main() {
-  var en = Envelop(Sncbrycx('430302195806251012'));
+  var en = RequestEnvelop(SncbrycxRequest('430302195806251012'));
   print(en.toXmlString());
+
+  var xml = parse(test);
+  var env = findChild(xml, 'Envelope');
+  if (env != null) {
+    var header = findChild(env, 'Header');
+    print(Resultable.convert(header));
+    var body = findChild(env, 'Body');
+    var business = findChild(body, 'business');
+    print(Resultable.convert(business));
+  }
+
+  var res = ResponseEnvelop.fromXmlString<SncbrycxResponse>(test);
+  print('${res.header.sessionID}|${res.header.message}');
+  print('${res.body.result}|${res.body.row_count}');
+  for (var item in res.body.querylist) {
+    print('${item.name}|${item.idcard}|${item.sbjg}');
+  }
 }
+
+var test = '''
+<?xml version="1.0" encoding="GBK"?>
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" soap:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+  <soap:Header>
+    <result sessionID="DpPZb8mZ0qgv08kN26LyKmm1yDz4nn7QvXxh2VD32vDvgvQ2zw14!-23337339!1530701497001"/>
+    <result message=""/>
+  </soap:Header>
+  <soap:Body>
+    <out:business xmlns:out="http://www.molss.gov.cn/">
+      <result result="" />
+      <resultset name="querylist">
+        <row aac003="徐斌" rown="1" aac008="2" aab300="湖南省社会保险管理服务局" sac007="101" aac031="3" aac002="430302195806251012" />
+      </resultset>
+      <result row_count="1" />
+      <result querysql="select * from (select /*+FIRST_ROWS(500)*/row_.* ,rownum rown  from (select aac002,aac003,aab300,aac031,sac007,aac008 from (select aac002, aac003, aab300, aac031, sac007, aac008, sab100
+  from ac01, ab50, ac02
+ where ac01.aac001 = ac02.aac001
+   and ac01.aab034 = ab50.aab034
+   --and ac01.aac002 = &apos;433022197609160913&apos;
+union all
+select a.aac002,
+       a.aac003,
+       decode(b.aab324,&apos;430101&apos;,&apos;长沙市&apos;,&apos;430102&apos;,&apos;芙蓉区&apos;,&apos;430103&apos;,&apos;天心区&apos;,&apos;430104&apos;,&apos;岳麓区&apos;,&apos;430105&apos;,&apos;开福区&apos;,&apos;430111&apos;,&apos;雨花区&apos;,&apos;其它&apos;),
+       b.aac031,
+       decode((select aab019 from ab01_css where aab001 = b.aab001),
+              &apos;73&apos;,
+              &apos;102&apos;,
+              &apos;101&apos;) sac007,
+       a.aac008,
+       b.aab001
+  from ac01_css a, ac02_css b
+ where a.aac001 = b.aac001) where ( aac002 = &apos;430302195806251012&apos;) and 1=1) row_ where rownum &lt;(501)) where rown &gt;=(1) " />
+    </out:business>
+  </soap:Body>
+</soap:Envelope>
+''';
